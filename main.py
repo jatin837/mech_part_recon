7266b5d764d5531ce7a8b8f6d2288b418da35e73
dj4t9n <pandeyjatin47@gmail.com>
2021-06-24 13:44:18 +0530

    STATUS: WIP constructing model

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import os
 import json
 
+import tensorflow as tf
+
+from tf.keras.models import Sequential
+from tf.keras.utils import to_categorical
+from tf.keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
+
 labels_dir: str = os.path.abspath("./labels.json")
 
 with open(labels_dir, 'r') as f:
@@ -8,5 +14,10 @@ with open(labels_dir, 'r') as f:
 
 dat_dir: str = os.path.abspath("./dat/blnw-images-224")
 categories: list = os.listdir(dat_dir)
+
+## TODO :
+## Creation of CNN. Sequential Model
+
+model = Sequential()
 
 
