58a82848544c7fc3335cbc2df38279d9c95b91fe
dj4t9n <pandeyjatin47@gmail.com>
2021-06-24 13:29:41 +0530

    load labels into memory

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
 import json
 
+labels_dir: str = os.path.abspath("./labels.json")
+
+with open(labels_dir, 'r') as f:
+    labels: dict = json.load(f)
+
 dat_dir: str = os.path.abspath("./dat/blnw-images-224")
 categories: list = os.listdir(dat_dir)
 
-print(categories)
+
