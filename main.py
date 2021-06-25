6539520ccbde01cc0d3273a6536a590b5f6e0f27
dj4t9n <pandeyjatin47@gmail.com>
2021-06-25 10:59:05 +0530

    create class for imgs

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import os
 import json
 import cv2
-import tensorflow as tf
+#import tensorflow as tf
+import numpy as np
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
+#from tensorflow.keras.models import Sequential
+#from tensorflow.keras.utils import to_categorical
+#from tensorflow.keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
 
 labels_dir: str = os.path.abspath("./labels.json")
+
+class Img(object):
+    def __init__(self, path: str, label: str, data: np.array):
+        self.path = path
+        self.label = label
+        self.data = data
+    
+    def __repr__(self):
+        return f"at {self.path}, {self.label}"
 
 with open(labels_dir, 'r') as f:
     labels: dict = json.load(f)
@@ -21,12 +31,19 @@ train_imgs: dict = {}
 test_imgs: dict = {}
 
 for cat in categories:
+    train_imgs[cat] = []
+    test_imgs[cat] = []
     imgs = os.listdir(f"{dat_dir}/{cat}") 
     part = int(_train*len(imgs))
-    train_imgs[cat] = imgs[:part]
-    test_imgs[cat] = imgs[part:]
-
-print(train_imgs, test_imgs)
+    for img in imgs[:part]:
+        img_path = os.path.join(dat_dir, cat, img)
+        img_to_dat = cv2.imread(img_path)
+        print(img_to_dat)
+        img_obj = Img(os.path.abspath(img_path), cat, cv2.imread(img_to_dat))
+        train_imgs[cat].append(img_obj)
+    for img in imgs[part:]:
+        img_obj = Img(os.path.abspath(img), cat, cv2.imread(os.path.abspath(img)))
+        test_imgs[cat].append(img_obj)
 
 #model:Sequential = Sequential()
 
