ae171a0215db6404f7a294d57f0b882171dfd9af
dj4t9n <pandeyjatin47@gmail.com>
2021-06-25 09:18:40 +0530

    lack of clarity, how do i train data

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import json
-
+import cv2
 import tensorflow as tf
 
 from tf.keras.models import Sequential
@@ -15,9 +15,10 @@ with open(labels_dir, 'r') as f:
 dat_dir: str = os.path.abspath("./dat/blnw-images-224")
 categories: list = os.listdir(dat_dir)
 
-## TODO :
-## Creation of CNN. Sequential Model
+_train: float = 0.75
+_total: float= 1.00
+train_imgs: dict = {}
+test_imgs: dict = {}
 
-model = Sequential()
-
-
+for cat in categories:
+   pass ## TODO -> for each category, store dictionary of each image along with it's label
