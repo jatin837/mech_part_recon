c9cc9516f1fac3e1c940f2a0cfb2b6875fc2ed71
dj4t9n <pandeyjatin47@gmail.com>
2021-06-25 10:35:33 +0530

    add train and test imgs

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import json
 import cv2
 import tensorflow as tf
 
-from tf.keras.models import Sequential
-from tf.keras.utils import to_categorical
-from tf.keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
 
 labels_dir: str = os.path.abspath("./labels.json")
 
@@ -21,17 +21,20 @@ train_imgs: dict = {}
 test_imgs: dict = {}
 
 for cat in categories:
-   pass ## TODO -> for each category, store dictionary of each image along with it's label
+    imgs = os.listdir(f"{dat_dir}/{cat}") 
+    part = int(_train*len(imgs))
+    train_imgs[cat] = imgs[:part]
+    test_imgs[cat] = imgs[part:]
 
+print(train_imgs, test_imgs)
 
+#model:Sequential = Sequential()
 
-model:Sequential = models.Sequential()
-
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10))
+#model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+#model.add(layers.MaxPooling2D((2, 2)))
+#model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+#model.add(layers.MaxPooling2D((2, 2)))
+#model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+#model.add(layers.Flatten())
+#model.add(layers.Dense(64, activation='relu'))
+#model.add(layers.Dense(10))
