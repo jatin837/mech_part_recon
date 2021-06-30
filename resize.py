b35c3bd2d94f7d616922e49a5fad66b6a8f2b0e1
dj4t9n <pandeyjatin47@gmail.com>
2021-06-30 12:36:06 +0530

    script did its work to resize each image

--- resize.py
+++ resize.py
@@ -7,4 +7,19 @@ labels_dir = os.path.abspath("./labels.json")
 labels = load_labels(labels_dir)
 
 
-data_dir = "/home/dj4t9n/dev/mech_part_recon/dat/resized_data"
+resized_dir = "/home/dj4t9n/dev/mech_part_recon/dat/resized_data"
+data_dir = "/home/dj4t9n/dev/mech_part_recon/dat/blnw-images-224"
+
+categories = labels.keys()
+
+count = 0
+for cat in categories:
+    for img in os.listdir(f"{data_dir}/{cat}"):
+        img_abs_path = os.path.join(data_dir, cat, img)
+        count = count + 1
+        print(count)
+        temp = cv2.imread(img_abs_path, 0)
+        resized_temp = cv2.resize(temp, (64, 64))
+        cv2.imwrite(f"{resized_dir}/{cat}/{str(count)}.png", resized_temp)
+
+print("DONE")
