bef144d7b30f27854641c96725bedd09a2f3477e
dj4t9n <pandeyjatin47@gmail.com>
2021-06-30 10:10:37 +0530

    did not work
    same problem
    
    program crashes

--- main.py
+++ main.py
@@ -8,13 +8,22 @@ def main() -> ():
 
     dat_dir: str = os.path.abspath("./dat/blnw-images-224")
     categories: list = os.listdir(dat_dir)
-
+    img_array = []
+    labels_array = []
+    img_array_file = os.path.abspath("./imgs.npy")
+    img_labels_file = os.path.abspath("./labels.npy")
     for cat in categories:
         imgs_list = os.listdir(f"{dat_dir}/{cat}")
         for img in imgs_list:
             img_path = os.path.join(dat_dir, cat, img)
             print(f'loading {img_path}')
             img_to_dat = cv2.imread(img_path, 0)/255.0
+            img_array.append(img_to_dat)
+            labels_array.append(labels[cat])
+    
+    img_array = np.array(img_array)
+    labels_array = np.array(labels_array)
+
 
 
 if __name__ == "__main__":
